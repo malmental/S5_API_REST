644ffb4f8b5337895f8a45a09d941b2026026c85
malmental <orlandorojasoyarzun@gmail.com>
2026-04-01 13:52:20 +0200

    fix: use unix socket for PHP-FPM communication

--- docker/start.sh
+++ docker/start.sh
@@ -9,6 +9,8 @@ php artisan passport:install --force
 # Generar documentación (si scribe está instalado)
 php artisan scribe:generate 2>/dev/null || true
 
-# Iniciar servicios
-php-fpm -D
+# Iniciar PHP-FPM con socket Unix
+php-fpm --fpm-config /usr/local/etc/php-fpm.d/www.conf -D
+
+# Iniciar Nginx
 nginx -g "daemon off;"
