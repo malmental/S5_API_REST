d98ce59a68539932e0054bb1acbaaea00894b43a
malmental <orlandorojasoyarzun@gmail.com>
2026-04-09 21:00:49 +0200

    trying to solve collapsed endpoints to show descriptions

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ Route::get('/', function () {
 Route::get('/docs', function () {
     $html = file_get_contents(public_path('docs/index.html'));
     $html = str_replace('../docs/', './', $html);
+    $html = str_replace('..\/docs\/', './', $html);
     return response($html)->header('Content-Type', 'text/html');
 });
 
